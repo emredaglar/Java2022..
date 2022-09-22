5de3d6443c0ebfbbef74dc9f82e53e968e826a4d
[firstname lastname] <[valid-email]>
2022-09-22 09:52:11 +0300

    deneme eklendi

---
+++ src/com/company/Deneme.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Deneme {
+    public static void main(String[] args) {
+        int sayi11 = 20;
+        int sayi2 = 25;
+        int sayi3 = 5;
+        int enBuyuk = sayi11;
+        if (enBuyuk < sayi2) {
+            enBuyuk = sayi2;
+        }
+
+        System.out.println(enBuyuk);
+    }
+}
